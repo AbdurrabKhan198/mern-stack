b59c7d0659529620a2fadd2c9d25e98e704815bb
AbdurrabKhan198 <abdurrabkhan709@gmail.com>
2025-07-14 15:22:39 +0530

    git

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 
 app = express();
+const router = require("./router/auth-router")
 
-app.get('/', (req, res) =>{
-    res.status(200).send('Hello, World!');
-});
 
-app.get('/register', (req, res) => {
-    res.status(200).send("Welcome to register page")
-});
+// mount the Router: To use the router in your main express app, you can "mount" it at a specific URL prefix
+app.use("/api/auth", router)
+
+// all the get files code has been moved into router and we are using by exports
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
